45fe6b6fc85ba074c0a7362b6a56d59f0c0db115
RobinDev <contact@piedweb.com>
2022-02-23 18:07:53 +0100

    fix test

--- Tests/PuphpeteerProviderTest.php
+++ Tests/PuphpeteerProviderTest.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 use PiedWeb\Google\Provider\Puphpeteer;
+use PiedWeb\Google\Provider\PuphpeteerExtractor;
 
 final class PuphpeteerProviderTest extends TestCase
 {
     public function testIt(): void
     {
         $provider = new Puphpeteer();
+        $extractor = new PuphpeteerExtractor($provider);
         $provider->instantiate();
         $rawHtml = $provider->get('https://www.google.fr/search?q=pied+web');
 
         $this->assertStringContainsString('piedweb.com', $rawHtml);
 
-        $results = $provider->getOrganicResults();
+        $results = $extractor->getOrganicResults();
         $this->assertSame('https://piedweb.com/', $results[0]->url);
 
         $this->assertTrue(count($results) > 2);
